c17386c711efa5192d215178a43b6d4f1126635a
Jan Justi <9977169+janjusti@users.noreply.github.com>
2019-06-30 10:51:23 -0300

    base created (based on dppa 0.1.2)

---
+++ dcma/_src/_core_methods.py
@@ -0,0 +1,12 @@
+class CoreMethods:
+    def start_main(self):
+        pass
+    
+    def start_run(self, target_path, searchable_keyphrase):
+        pass
+    
+    def start_export(self, results_df_list, report_type, report_name, report_path):
+        pass
+    
+    def set_debug_mode(self, isActive):
+        pass
